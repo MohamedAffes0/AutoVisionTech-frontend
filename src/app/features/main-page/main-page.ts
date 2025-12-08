4bb3f34c77e3e8fe48211e8825191e3287ec1a62
mayssabenmrad <148723874+mayssabenmrad@users.noreply.github.com>
2025-12-08 13:25:19 +0100

    feat(homepage): implement dynamic filtering

--- src/app/features/main-page/main-page.ts
+++ src/app/features/main-page/main-page.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CarCrad } from '../../shared/components/car-crad/car-crad';
-import { CarFilter } from '@shared/components/car-filter/car-filter';
+import { CarFilter, SearchFilters, SortByType } from '@shared/components/car-filter/car-filter';
 import { Hero } from "@shared/components/hero/hero";
-import { Car, CarsResponse } from 'src/app/core/models';
+import { Car, CarsResponse, CarFilters } from 'src/app/core/models';
 import { CarService } from 'src/app/core/services/car.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
-  imports: [CarCrad, CarFilter, Hero, CommonModule],
-  templateUrl: './main-page.html',
-  styleUrl: './main-page.css',
   standalone: true,
+  imports: [CarCrad, CarFilter, Hero, CommonModule, FormsModule],
+  templateUrl: './main-page.html',
+  styleUrls: ['./main-page.css']
 })
 export class MainPage implements OnInit {
   cars: Car[] = [];
@@ -23,21 +24,41 @@ export class MainPage implements OnInit {
     this.loadCars();
   }
 
-  loadCars() {
+  //retrieve cars and apply filters
+  loadCars(filters?: CarFilters) {
     this.loading = true;
-
-    this.carService.getCars(1, 20).subscribe({
+    this.carService.getCars(1, 100, filters).subscribe({
       next: (res: CarsResponse) => {
-        console.log("Cars loaded:", res);
         this.cars = res.items;
         this.loading = false;
       },
       error: (err) => {
-        console.error("Error loading cars:", err);
+        console.error('Error loading cars:', err);
         this.loading = false;
       }
     });
   }
-}
 
+  //filtering by price, brand, kilometerAge and year
+  onFiltersChanged(filters: SearchFilters) {
+    const apiFilters: CarFilters = {};
+
+    if (filters.minPrice != null) apiFilters.minPrice = filters.minPrice.toString();
+    if (filters.maxPrice != null) apiFilters.maxPrice = filters.maxPrice.toString();
+
+    switch (filters.sortBy) {
+      case 'price-asc': apiFilters.sortByPrice = 'asc'; break;
+      case 'price-desc': apiFilters.sortByPrice = 'desc'; break;
+      case 'year-desc': apiFilters.sortByYear = 'desc'; break;
+      case 'mileage-asc': apiFilters.sortByKilometerAge = 'asc'; break;
+      case 'mileage-desc': apiFilters.sortByKilometerAge = 'desc'; break;
+    }
+
+    if (filters.searchText) {
+      apiFilters.brand = filters.searchText;
+    }
+    console.log('Filters going to API:', apiFilters);
+    this.loadCars(apiFilters);
+  }
+}
 
